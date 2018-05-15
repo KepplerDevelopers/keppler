c721dd7f40e9c5039fb6b250a9e2027bc1fc8685
Luis Perez <luis.prz7@gmail.com>
2018-05-15 11:19:58 -0400

    cambios

--- lib/keppler/cli.rb
+++ lib/keppler/cli.rb
@@ -7,7 +7,7 @@ module Keppler
     desc 'new NAME', 'Create a new keppler app'
 
     def new(project_name)
-      system("scp -r $GEM_HOME/keppler-#{Keppler::VERSION}/installer/core #{project_name}")
+      system("scp -r $GEM_HOME/gems/keppler-#{Keppler::VERSION}/installer/core #{project_name}")
     end
 	end
 end
