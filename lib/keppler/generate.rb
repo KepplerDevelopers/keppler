52a1d6c0a34bc4036662f877a00ef2b8a72f89a7
Luis Perez <luis.prz7@gmail.com>
2018-05-15 15:15:23 -0400

    cambios

--- lib/keppler/generate.rb
+++ lib/keppler/generate.rb
@@ -6,7 +6,7 @@ module Keppler
     desc 'module', 'Create a new keppler module'
     def module(*params)
       module_name = params[0]
-      fields = params[2..params.length].join(" ")
+      fields = params[1..params.length].join(" ")
       system("rails g keppler_scaffold #{module_name} #{fields} position:integer deleted_at:datetime:index -y")
     end
   end
