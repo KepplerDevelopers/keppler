2f839b9dfc2bdcd43e66bcd6510c3139e24bf1d0
Luis Perez <luis.prz7@gmail.com>
2018-05-15 15:46:09 -0400

    cambios

--- lib/keppler/cli.rb
+++ lib/keppler/cli.rb
@@ -6,6 +6,19 @@ require_relative 'delete'
 
 module Keppler
 	class Cli < Thor
+    long_desc <<-HELLO_WORLD
+
+    `hello NAME` will print out a message to the person of your choosing.
+
+    Brian Kernighan actually wrote the first "Hello, World!" program
+    as part of the documentation for the BCPL programming language
+    developed by Martin Richards. BCPL was used while C was being
+    developed at Bell Labs a few years before the publication of
+    Kernighan and Ritchie's C book in 1972.
+
+    http://stackoverflow.com/a/12785204
+    HELLO_WORLD
+
     desc 'new NAME', 'Create a new keppler app'
 
     def new(project_name)
