674193e300b9c1b57e9765b26d202dcdcaa4cdde
Luis Perez <luis.prz7@gmail.com>
2018-05-15 11:29:11 -0400

    cambios

--- lib/keppler/cli.rb
+++ lib/keppler/cli.rb
@@ -9,43 +9,42 @@ module Keppler
     def new(project_name)
       system("scp -r $GEM_HOME/gems/keppler-#{Keppler::VERSION}/installer/core #{project_name}")
       puts '> Created project'
-      system("cd #{project_name}")
-      system("bundle install")
+      system("cd #{project_name} && bundle install")
       puts '> Installed dependences'
       system("scp -r $GEM_HOME/gems/keppler-#{Keppler::VERSION}/installer/db_conf/conf.yml config/secrets.yml")
 
-      puts "----------------------------------------------------------"
-      puts "Database name"
-      puts "----------------------------------------------------------"
-      db_name = STDIN.gets.chomp
-      puts "----------------------------------------------------------"
-      puts "Database username"
-      puts "----------------------------------------------------------"
-      db_username = STDIN.gets.chomp
-      puts "----------------------------------------------------------"
-      puts "Database password"
-      puts "----------------------------------------------------------"
-      db_password = STDIN.gets.chomp
+      # puts "----------------------------------------------------------"
+      # puts "Database name"
+      # puts "----------------------------------------------------------"
+      # db_name = STDIN.gets.chomp
+      # puts "----------------------------------------------------------"
+      # puts "Database username"
+      # puts "----------------------------------------------------------"
+      # db_username = STDIN.gets.chomp
+      # puts "----------------------------------------------------------"
+      # puts "Database password"
+      # puts "----------------------------------------------------------"
+      # db_password = STDIN.gets.chomp
 
 
-      db_conf = File.readlines("config/secrets.yml")
-      new_file = []
-      db_conf.each do |line|
-        if line.eql?("    :name: database\n")
-          line = "    :name: #{db_name}\n"
-        elsif line.eql?("    :username: username\n")
-          line = "    :username: #{db_username}\n"
-        elsif line.eql?("    :password: password\n")
-          line = "    :password: #{db_password}\n"
-        end
-        new_file.push(line)
-      end
-      new_file = new_file.join("")
-      File.write("config/secrets.yml", new_file)
-      puts '> Configured database'
+      # db_conf = File.readlines("config/secrets.yml")
+      # new_file = []
+      # db_conf.each do |line|
+      #   if line.eql?("    :name: database\n")
+      #     line = "    :name: #{db_name}\n"
+      #   elsif line.eql?("    :username: username\n")
+      #     line = "    :username: #{db_username}\n"
+      #   elsif line.eql?("    :password: password\n")
+      #     line = "    :password: #{db_password}\n"
+      #   end
+      #   new_file.push(line)
+      # end
+      # new_file = new_file.join("")
+      # File.write("config/secrets.yml", new_file)
+      # puts '> Configured database'
 
-      system("rake db:create db:migrate db:seed")
-      puts '> Created database'
+      # system("rake db:create db:migrate db:seed")
+      # puts '> Created database'
       puts "#{project_name} has been created"
     end
 	end
