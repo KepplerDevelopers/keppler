cb6fb69f594bea4ae5341e7df3e380617a753907
Luis Perez <luis.prz7@gmail.com>
2018-05-15 15:05:20 -0400

    cambios

--- lib/keppler/generate.rb
+++ lib/keppler/generate.rb
@@ -4,11 +4,11 @@ module Keppler
   class Generate < Thor
 
     desc 'module', 'Create a new keppler module'
-    def module(params)
-      byebug
-      fields = params
-      fields = fields[2..fields.length]
-      system("rails g keppler_scaffold #{params[0]} #{fields} position:integer deleted_at:datetime:index -y")
+    def module(*params)
+      params = params.split(" ")
+      module_name = params[0]
+      fields = params[2..params.length]
+      system("rails g keppler_scaffold #{module_name} #{fields} position:integer deleted_at:datetime:index -y")
     end
   end
 end
