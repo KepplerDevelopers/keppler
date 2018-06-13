14e6bde526ed998bf4f8f3f41f54b9bf89de5716
Luis Perez <luis.prz7@gmail.com>
2018-06-13 16:20:19 -0400

    cambios

--- lib/keppler/cli.rb
+++ lib/keppler/cli.rb
@@ -3,7 +3,6 @@ require 'thor'
 require "keppler/version"
 require_relative 'add'
 require_relative 'delete'
-require_relative 'rocket'
 
 module Keppler
 	class Cli < Thor
@@ -123,10 +122,27 @@ module Keppler
     desc "delete module NAME attr:type attr:type", "Delete a keppler module"
     subcommand "delete", Delete
 
-    desc 'rocket new NAME', 'Create a new keppler plugin'
-    subcommand "new", Rocket
+    desc 'rocket_new NAME', 'Create a new keppler plugin'
 
-    desc 'rocket build NAME', 'Build a keppler plugin'
-    subcommand "build", Rocket
+    def rocket_new(plugin_name)
+      plugin_name = plugin_name.downcase
+      system("rails plugin new keppler_#{plugin_name} --mountable")
+      puts "> Created scaffold"
+      system("cd keppler_#{plugin_name} && scp -r $GEM_HOME/gems/keppler-#{Keppler::VERSION}/installer/plugins/generators lib/generators")
+      puts "> Installed generators"
+      system("mkdir keppler_#{plugin_name}/app/views/keppler_#{plugin_name}")
+      system("mkdir keppler_#{plugin_name}/app/views/keppler_#{plugin_name}/admin")
+      system("scp -r $GEM_HOME/gems/keppler-#{Keppler::VERSION}/installer/plugins/layouts keppler_#{plugin_name}/app/views/keppler_#{plugin_name}/admin/layouts")
+      system("ruby $GEM_HOME/gems/keppler-#{Keppler::VERSION}/installer/plugins/install.rb keppler_#{plugin_name}")
+      system("mkdir keppler_#{plugin_name}/app/policies")
+      puts "> Installed policies"
+      puts "#{plugin_name} has been created"
+    end
+
+    desc 'rocket_build NAME', 'Build a keppler plugin'
+
+    def rocket_build(plugin_name)
+      system("zip -r #{plugin_name}.rocket #{plugin_name}/*")
+    end
 	end
 end
