cf021a40cb11011e29ffdb056e2b9420fd63d7c0
luprz <luis.prz7@gmail.com>
2018-10-17 08:02:33 -0400

    Cambios en los generadores

--- lib/keppler/delete.rb
+++ lib/keppler/delete.rb
@@ -8,7 +8,7 @@ module Keppler
       rocket_name = params[0]
       module_name = params[1]
       fields = params[1..params.length].join(" ")
-      system("rails d keppler_delete_module #{rocket_name} #{module_name} -y")
+      system("rails g keppler_delete_module #{rocket_name} #{module_name} -y")
     end
   end
 end
