1c220da950ec12be3be73b505966b58be2bbdef4
luprz <luis.prz7@gmail.com>
2019-01-28 13:43:09 -0400

    removed log file

--- lib/keppler/version.rb
+++ lib/keppler/version.rb
@@ -1,11 +1,3 @@
 module Keppler
-<<<<<<< HEAD
-<<<<<<< HEAD
   VERSION = "2.1.16"
-=======
-  VERSION = "2.1.15"
->>>>>>> 3a47a9e9183b351a7218d9ab7b5e02cd38af52dc
-=======
-  VERSION = "2.1.15"
->>>>>>> c1ca93591275b3012a39f53dbb4d1a07c87d57e5
 end
