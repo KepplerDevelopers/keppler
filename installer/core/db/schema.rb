a55e9c5f678709239ccaaba4af901dc2b24a9531
luprz <luis.prz7@gmail.com>
2019-06-09 10:09:28 -0500

    publish 2.0.21

--- installer/core/db/schema.rb
+++ installer/core/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_144238) do
+ActiveRecord::Schema.define(version: 2019_06_03_011323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,15 +110,6 @@ ActiveRecord::Schema.define(version: 2018_09_04_144238) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["deleted_at"], name: "index_keppler_capsules_capsules_on_deleted_at"
-  end
-
-  create_table "keppler_frontend_partials", force: :cascade do |t|
-    t.string "name"
-    t.integer "position"
-    t.datetime "deleted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["deleted_at"], name: "index_keppler_frontend_partials_on_deleted_at"
   end
 
   create_table "keppler_frontend_themes", force: :cascade do |t|
